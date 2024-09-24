3d8e6b03e33a147fbedde520d0c68f0d2380ead3
Rajprudvin <prudvirajn@gmail.com>
2024-09-24 22:24:50 +0530

    module

---
+++ outputs.tf
@@ -0,0 +1,7 @@
+output "vpc_id" {
+    value = module.vpc.vpc_id
+}
+#below block gives more output it has been disturbing so i have comment
+# output "az_info" {
+#     value = module.vpc.az_info
+# }
